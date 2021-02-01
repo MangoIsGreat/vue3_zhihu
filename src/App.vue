ca14caa47d53c89e0dff780a57a6774203c09985
zp <859247268@qq.com>
2021-02-02 01:18:31 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,121 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <!-- <h1>{{ count }}</h1>
+    <h1>{{ double }}</h1> -->
+    <!-- <ul>
+      <li v-for="number in numbers" :key="number">
+        <h1>{{ number }}</h1>
+      </li>
+    </ul>
+    <h1>{{ person.name }}</h1>
+    <button @click="increase">+1</button> -->
+    <div>{{ x }}</div>
+    <div>{{ y }}</div>
+
+    <modal />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import useMousePosition from "./hooks/useMousePosition";
+import useURLLoader from "./hooks/useURLLoader";
+import modal from "./components/modal.vue";
 
-export default defineComponent({
-  name: 'App',
+import {
+  ref,
+  computed,
+  reactive,
+  toRefs,
+  onMounted,
+  onUpdated,
+  onRenderTriggered,
+  watch,
+  toRef,
+  onUnmounted,
+} from "vue";
+
+interface DataProps {
+  count: number;
+  double: number;
+  numbers: number[];
+  person: { name?: string };
+}
+
+interface DogMessage {
+  mesage: string;
+  status: number;
+}
+
+interface CatResult {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+export default {
+  name: "App",
   components: {
-    HelloWorld
-  }
-});
+    modal
+  },
+  setup() {
+    const data: DataProps = reactive({
+      count: 0,
+      double: 0,
+      numbers: [0, 1, 2],
+      person: {},
+    });
+
+    const greetings = ref("");
+    const updateGreeting = () => {
+      greetings.value += "Hello!";
+    };
+
+    watch([greetings, () => data.count], (newValue, oldValue) => {
+      document.title = "updated" + greetings.value + data.count;
+    });
+
+    data.numbers[0] = 5;
+
+    data.person.name = "viking";
+
+    const refData = toRefs(data);
+
+    // const x = ref(0);
+    // const y = ref(0);
+
+    // const updateMouse = (e: MouseEvent) => {
+    //   x.value = e.pageX;
+    //   y.value = e.pageY;
+    // };
+
+    // onMounted(() => {
+    //   document.addEventListener("click", updateMouse);
+    // });
+
+    // onUnmounted(() => {
+    //   document.removeEventListener('click', updateMouse);
+    // });
+
+    const { x, y } = useMousePosition();
+
+    const { result, loading, loaded } = useURLLoader<CatResult[]>(
+      "http://xxx.com"
+    );
+
+    watch(result, () => {
+      if (result.value) {
+        console.log("value", result.value[0].url);
+      }
+    });
+
+    return {
+      ...refData,
+      x,
+      y,
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <teleport to='#modal'>
+      <div id="center">
+          <h2>this is a modal</h2>
+      </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+#center {
+    width: 200px;
+    height: 200px;
+    border: 2px solid black;
+    background-color: #fff;
+    position: fixed;
+    left: 50%;
+}
+</style>
