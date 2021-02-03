d61fb64c4e0bdbc706b0013d8344c9fd25a541c4
zp <859247268@qq.com>
2021-02-04 03:09:57 +0800

    feat: ColumnLis组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,40 @@
 <template>
-  <div>
-    <!-- <h1>{{ count }}</h1>
-    <h1>{{ double }}</h1> -->
-    <!-- <ul>
-      <li v-for="number in numbers" :key="number">
-        <h1>{{ number }}</h1>
-      </li>
-    </ul>
-    <h1>{{ person.name }}</h1>
-    <button @click="increase">+1</button> -->
-    <div>{{ x }}</div>
-    <div>{{ y }}</div>
-
-    <modal />
+  <div class="container">
+    <column-list :list="list"></column-list>
   </div>
 </template>
 
 <script lang="ts">
-import useMousePosition from "./hooks/useMousePosition";
-import useURLLoader from "./hooks/useURLLoader";
-import modal from "./components/modal.vue";
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 
-import {
-  ref,
-  computed,
-  reactive,
-  toRefs,
-  onMounted,
-  onUpdated,
-  onRenderTriggered,
-  watch,
-  toRef,
-  onUnmounted,
-} from "vue";
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "test1专栏",
+    avatar: "http://xxx.com",
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "test2专栏",
+    avatar: "http://xxx.com",
+  },
+];
 
-interface DataProps {
-  count: number;
-  double: number;
-  numbers: number[];
-  person: { name?: string };
-}
-
-interface DogMessage {
-  mesage: string;
-  status: number;
-}
-
-interface CatResult {
-  id: string;
-  url: string;
-  width: number;
-  height: number;
-}
-
-export default {
+export default defineComponent({
   name: "App",
   components: {
-    modal
+    ColumnList,
   },
   setup() {
-    const data: DataProps = reactive({
-      count: 0,
-      double: 0,
-      numbers: [0, 1, 2],
-      person: {},
-    });
-
-    const greetings = ref("");
-    const updateGreeting = () => {
-      greetings.value += "Hello!";
-    };
-
-    watch([greetings, () => data.count], (newValue, oldValue) => {
-      document.title = "updated" + greetings.value + data.count;
-    });
-
-    data.numbers[0] = 5;
-
-    data.person.name = "viking";
-
-    const refData = toRefs(data);
-
-    // const x = ref(0);
-    // const y = ref(0);
-
-    // const updateMouse = (e: MouseEvent) => {
-    //   x.value = e.pageX;
-    //   y.value = e.pageY;
-    // };
-
-    // onMounted(() => {
-    //   document.addEventListener("click", updateMouse);
-    // });
-
-    // onUnmounted(() => {
-    //   document.removeEventListener('click', updateMouse);
-    // });
-
-    const { x, y } = useMousePosition();
-
-    const { result, loading, loaded } = useURLLoader<CatResult[]>(
-      "http://xxx.com"
-    );
-
-    watch(result, () => {
-      if (result.value) {
-        console.log("value", result.value[0].url);
-      }
-    });
-
     return {
-      ...refData,
-      x,
-      y,
+      list: testData,
     };
   },
-};
+});
 </script>
 
 <style>
